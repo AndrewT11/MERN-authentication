60c89d16f04166026f5fe9dc8d781a48695110d0
Andrew Tran <tranandrew@gmail.com>
2022-06-09 18:55:05 -0500

    basic home page created. Frontend complete

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container text-center" style={{ marginTop: "11rem" }}>
+      <div className="alert alert-primary p-5">
+        <h1>Welcome to the MERN authenticator.</h1>
+        <h3>This app will utilize Bootstrap and Material UI for its design</h3>
+        <h3>This page is currently a work in progress.</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
